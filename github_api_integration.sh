a89a4a4aa526875bce4d3cb7aa1c204bc74782ba
festusebin <ebinfestus@gmail.com>
2024-04-05 10:47:02 +0100

    added shell scripting

---
+++ github_api_integration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#########################
+
+# Author: Festus Ebin
+# Date: 02/04/2024
+#
+# This script outputs the monitoring report of AWS Resources
+#
+# version: v1
+
+#########################
